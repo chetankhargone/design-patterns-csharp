ae796203f75660fd4d585025a2878bd9ed7a8aa8
Chetan Gupta <chetan.gupta21@gmail.com>
2018-07-13 23:31:57 +0530

    added UnitOfWork - to handle db transaction

--- repository-pattern/customer.db.ef/CustomerContext.cs
+++ repository-pattern/customer.db.ef/CustomerContext.cs
@@ -3,12 +3,28 @@ using System.Data.Entity;
 
 namespace customer.db.ef
 {
-    public class CustomerContext :
-        DbContext
+   public class CustomerContext :
+        DbContext, IUnitOfWork
     {
+        private DbContextTransaction _dbTransaction;
         protected override void OnModelCreating(DbModelBuilder modelBuilder)
         {
             base.OnModelCreating(modelBuilder);
+        }
+
+        public void InitTransaction()
+        {
+            _dbTransaction = this.Database.BeginTransaction();
+        }
+
+        public void Rollback()
+        {
+            _dbTransaction.Rollback();
+        }
+
+        public void CommitTransaction()
+        {
+            _dbTransaction.Commit();
         }
 
         #region DbSet
